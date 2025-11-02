05fb3abf57417d422c5c6b44c1ccc2a6b8b7ad5a
yjks <yj.kasai@gmail.com>
2025-11-02 23:03:01 +0900

    Retrievalについてハンズオン

---
+++ practice/practice3.py
@@ -0,0 +1,26 @@
+from langchain_google_genai.embeddings import GoogleGenerativeAIEmbeddings
+from langchain_community.utils.math import cosine_similarity
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.getenv("GOOGLE_API_KEY")
+
+# Googleの埋め込みモデルを設定
+embeddings_model = GoogleGenerativeAIEmbeddings(model="models/gemini-embedding-001")
+
+# テキストの埋め込みベクトルを取得
+embedding1 = embeddings_model.embed_query("AIはどのように機能しますか？")
+embedding2 = embeddings_model.embed_query("人工知能の仕組みを教えてください。")
+embedding3 = embeddings_model.embed_query("お天気はどうですか？")
+
+# ベクトルの次元数を確認
+print("埋め込みベクトルの次元数:", len(embedding1))
+
+# 埋め込みベクトル間のコサイン類似度を計算
+similarity_1_2 = cosine_similarity([embedding1], [embedding2])
+similarity_1_3 = cosine_similarity([embedding1], [embedding3])
+
+# 結果を表示
+print(f"similarity_1_2:{similarity_1_2}") 
+print(f"similarity_1_3:{similarity_1_3}") 
