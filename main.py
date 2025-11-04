2e382b0f7ada80ec80a77d780f3a64c806827ca7
yjks <yj.kasai@gmail.com>
2025-11-05 08:38:54 +0900

    骨組み

---
+++ main.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+def init_page() -> None:
+    st.set_page_config(
+        page_title="My Streamlit App",
+        page_icon=""
+    )
+
+def main() -> None:
+    init_page()
+
+    st.write("Hello, Streamlit!")
+
+    st.sidebar.success("Welcome to the sidebar!")
+
+    st.marlkdown(
+        """
+        # AIチャットアプリへようこそ！
+        
+        このアプリは、Google Geminiモデルを使用してユーザーと対話します。
+        サイドバーからオプションを選択し、チャットを開始してください。
+        """
+    )
+
+if __name__ == "__main__":
+    main()
